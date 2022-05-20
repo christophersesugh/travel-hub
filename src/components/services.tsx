a05c4bb71f1df12690c17ff685cdcb86536fdf01
Christopher Sesugh <christohybrid185@gmail.com>
2022-05-20 14:08:56 +0100

    Add service section

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { services } from "constants/services";
+import styled from "styled-components";
 
 export default function Services() {
-  return <div>serviceS</div>;
+  return (
+    <Section>
+      {services.map((service) => (
+        <div className="service">
+          <div className="icon">
+            <img src={service.icon} alt={service.title} />
+          </div>
+          <h3>{service.title}</h3>
+          <p>{service.subtitle}</p>
+        </div>
+      ))}
+    </Section>
+  );
 }
+
+const Section = styled.section`
+  padding: 5rem 0;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+  .service {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem;
+    background-color: aliceblue;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    transition: 0.3s ease-in-out;
+    &:hover {
+      transform: translateX(0.4rem) translateY(-1rem);
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+    .icon {
+      img {
+        height: 2.4rem;
+      }
+    }
+  }
+`;
---
+++ src/constants/services.tsx
@@ -0,0 +1,31 @@
+import services1 from "assets/service1.png";
+import services2 from "assets/service2.png";
+import services3 from "assets/service3.png";
+import services4 from "assets/service4.png";
+
+export const services = [
+  {
+    icon: services1,
+    title: "Lorem, ipsum dolor.",
+    subtitle:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, adipisci fuga! ",
+  },
+  {
+    icon: services2,
+    title: "Lorem, ipsum dolor.",
+    subtitle:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, adipisci fuga! ",
+  },
+  {
+    icon: services3,
+    title: "Lorem, ipsum dolor.",
+    subtitle:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, adipisci fuga! ",
+  },
+  {
+    icon: services4,
+    title: "Lorem, ipsum dolor.",
+    subtitle:
+      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia, adipisci fuga! ",
+  },
+];
