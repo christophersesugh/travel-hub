6da95565904721225bd2a6567942c8c9ebf29bfb
Christopher Sesugh <christohybrid185@gmail.com>
2022-05-24 12:45:28 +0100

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,13 +7,65 @@ export default function Footer() {
   return (
     <FooterContainer>
       <span>Copyright &copy; 2022 Travel hub. All rights reserved.</span>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+      <ul className="links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#recommend">Places</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+      </ul>
+      <ul className="social__links">
+        <li>
+          <a href="#home">
+            <BsLinkedin className="svg" />
+          </a>
+        </li>
+        <li>
+          <a href="#services">
+            <BsFacebook className="svg" />
+          </a>
+        </li>
+        <li>
+          <a href="#services">
+            <AiFillInstagram className="svg" />
+          </a>
+        </li>
       </ul>
     </FooterContainer>
   );
 }
 
-const FooterContainer = styled.footer``;
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #d0d8ff;
+  padding: 2.5rem;
+  border-radius: 0.5rem;
+  ul {
+    display: flex;
+    gap: 2rem;
+    li {
+      a {
+        color: black;
+        transition: 0.3s all linear;
+        &:hover {
+          color: #302ce9;
+        }
+      }
+      .svg {
+        font-size: 1.3rem;
+        transition: 0.3s all linear;
+        &:hover {
+          color: #302ce9;
+        }
+      }
+    }
+  }
+`;
